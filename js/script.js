f3496270a60f63726b1ff4e0fc3908ed2065be48
Agustin Panero <agustinpanero@MacBook-Pro-de-Agustin.local>
2024-11-19 19:35:49 +0100

    Contador Hecho

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-const contador = 0;
+let contador = parseInt(localStorage.getItem("contador"));
+contador++
 localStorage.setItem("contador", `${contador}`)
 const boton = document.getElementById("btnReestablecer");
 
 console.log(localStorage.getItem("contador"))
+
