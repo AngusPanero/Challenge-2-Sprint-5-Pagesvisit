5b6539b0718c6e84c0c21b837090afc221e8bc1b
Agustin Panero <agustinpanero@MacBook-Pro-de-Agustin.local>
2024-11-20 20:01:15 +0100

    Contador y Reseteo Hecho

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
-let contador = parseInt(localStorage.getItem("contador"));
-contador++
-localStorage.setItem("contador", `${contador}`)
+let contadorVisitas = parseInt(localStorage.getItem("contadorVisitas")) || 0; // el parseInt me devuelve en numero el string que me da el resultado de localStorage || 0 empezando desde cero
+contadorVisitas++
+localStorage.setItem("contadorVisitas", `${contadorVisitas}`)
+
+const visitasNumero = document.getElementById("contadorVisitas") // Muestro el Número en el navegador
+visitasNumero.innerHTML = `<p>${contadorVisitas}</p>`
+
 const boton = document.getElementById("btnReestablecer");
 
-console.log(localStorage.getItem("contador"))
+boton.addEventListener("click", () => {
+    contadorVisitas = 0; // Reseteo a 0 y luego reasigno mi contador para que se sobreescriba
+    localStorage.setItem("contadorVisitas", contadorVisitas.toString());
+    visitasNumero.innerHTML = `<p>${contadorVisitas}</p>`;
+});
+
+
+
+
+
+
+
 
